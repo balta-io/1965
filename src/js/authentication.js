c05b56c706fb683067a19f669e519e0643fc2617
André Baltieri <andrebaltieri@hotmail.com>
2016-09-02 13:45:19 -0300

    Adicionado exemplo completo de autenticação

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -1,20 +1,119 @@
-// Referências
-var provider = new firebase.auth.GithubAuthProvider();
-var authButton = document.getElementById('authButton');
+// Buttons
+var authEmailPassButton = document.getElementById('authEmailPassButton');
+var authFacebookButton = document.getElementById('authFacebookButton');
+var authTwitterButton = document.getElementById('authTwitterButton');
+var authGoogleButton = document.getElementById('authGoogleButton');
+var authGitHubButton = document.getElementById('authGitHubButton');
+var authAnonymouslyButton = document.getElementById('authAnonymouslyButton');
+var createUserButton = document.getElementById('createUserButton');
+var logOutButton = document.getElementById('logOutButton');
+
+// Inputs
+var emailInput = document.getElementById('emailInput');
+var passwordInput = document.getElementById('passwordInput');
+
+// Displays
 var displayName = document.getElementById('displayName');
 
-// Ao clicar no botão, autentica
-authButton.addEventListener('click', function () {
+// Criar novo usuário
+createUserButton.addEventListener('click', function () {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(emailInput.value, passwordInput.value)
+        .then(function () {
+            alert('Bem vindo ' + emailInput.value);
+        })
+        .catch(function (error) {
+            console.error(error.code);
+            console.error(error.message);
+            alert('Falha ao cadastrar, verifique o erro no console.')
+        });
+});
+
+// Autenticar com E-mail e Senha
+authEmailPassButton.addEventListener('click', function () {
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(emailInput.value, passwordInput.value)
+        .then(function (result) {
+            console.log(result);
+            displayName.innerText = 'Bem vindo, ' + emailInput.value;
+            alert('Autenticado ' + emailInput.value);
+        })
+        .catch(function (error) {
+            console.error(error.code);
+            console.error(error.message);
+            alert('Falha ao autenticar, verifique o erro no console.')
+        });
+});
+
+// Autenticar Anônimo
+authAnonymouslyButton.addEventListener('click', function () {
+    firebase
+        .auth()
+        .signInAnonymously()
+        .then(function (result) {
+            console.log(result);
+            displayName.innerText = 'Bem vindo, desconhecido';
+            alert('Autenticado Anonimamente');
+        })
+        .catch(function (error) {
+            console.error(error.code);
+            console.error(error.message);
+            alert('Falha ao autenticar, verifique o erro no console.')
+        });
+});
+
+// Autenticar com GitHub
+authGitHubButton.addEventListener('click', function () {
+    // Providers
+    var provider = new firebase.auth.GithubAuthProvider();
+    signIn(provider);
+});
+
+// Autenticar com Facebook
+authFacebookButton.addEventListener('click', function () {
+    // Providers
+    var provider = new firebase.auth.FacebookAuthProvider();
+    signIn(provider);
+});
+
+// Autenticar com Twitter
+authTwitterButton.addEventListener('click', function () {
+    // Providers
+    var provider = new firebase.auth.TwitterAuthProvider();
+    signIn(provider);
+});
+
+// Autenticar com Google
+authGoogleButton.addEventListener('click', function () {
+    // Providers
+    var provider = new firebase.auth.GoogleAuthProvider();
+    signIn(provider);
+});
+
+function signIn(provider) {
     firebase.auth()
         .signInWithPopup(provider)
         .then(function (result) {
+            console.log(result);
             var token = result.credential.accessToken;
             displayName.innerText = 'Bem vindo, ' + result.user.displayName;
-            authButton.disabled = true;
         }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
+            console.log(error);
+            alert('Falha na autenticação');
+        });
+}
+
+// Logout
+logOutButton.addEventListener('click', function () {
+    firebase
+        .auth()
+        .signOut()
+        .then(function () {
+            displayName.innerText = 'Você não está autenticado';
+            alert('Você se deslogou');
+        }, function (error) {
+            console.error(error);
         });
 });
